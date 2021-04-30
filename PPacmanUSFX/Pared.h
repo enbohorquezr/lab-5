22d4f512cfdc0ed2eb1e23c7d411232a5467fc66
erwin nolberto bohorquez reyes <erwinnolbertobohorquezreyes@hotmail.com>
2021-04-29 23:56:38 -0400

    Agregar archivos de proyecto.

---
+++ PPacmanUSFX/Pared.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "GameObject.h"
+class Pared :
+    public GameObject
+{
+public:
+    Pared(Texture* _paredTextura, int _posicionX, int _posicionY, int _ancho, int _alto, int _anchoPantalla, int _altoPantalla);
+       
+
+    
+
+};
+
